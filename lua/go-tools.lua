2bf5f549199f79105d147b2bbc14e0fcf22b2855
Huy Duong <huy.duongdinh@gmail.com>
2021-08-19 11:25:55 +0700

    Rename go command

--- lua/go-tools.lua
+++ lua/go-tools.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 function M.setup()
-  vim.cmd [[command! -nargs=* GoAddTag lua require("go-tools.tags").add(<f-args>)]]
-  vim.cmd [[command! -nargs=* GoRmTag lua require("go-tools.tags").remove(<f-args>)]]
-  vim.cmd [[command! GoClearTag lua require("go-tools.tags").clear()]]
+  vim.cmd [[command! -nargs=* GoTagsAdd lua require("go-tools.tags").add(<f-args>)]]
+  vim.cmd [[command! -nargs=* GoTagsRm lua require("go-tools.tags").remove(<f-args>)]]
+  vim.cmd [[command! GoTagsClear lua require("go-tools.tags").clear()]]
 
   vim.cmd [[command! GoTest lua require("go-tools.tests").run_test()]]
   vim.cmd [[command! GoTests lua require("go-tools.tests").run_tests()]]
   vim.cmd [[command! GoAddTest lua require("go-tools.tests").add_test()]]
   vim.cmd [[command! GoAddTests lua require("go-tools.tests").add_tests()]]
 
-  vim.cmd [[command! GoInstallDap lua require("go-tools.dap").install()]]
-  vim.cmd [[command! GoUninstallDap lua require("go-tools.dap").uninstall()]]
-  vim.cmd [[command! GoConfigDap lua require("go-tools.dap").config()]]
+  vim.cmd [[command! GoDapInstall lua require("go-tools.dap").install()]]
+  vim.cmd [[command! GoDapUninstall lua require("go-tools.dap").uninstall()]]
+  vim.cmd [[command! GoDapConfig lua require("go-tools.dap").config()]]
 
   vim.cmd [[command! GoDebugStart lua require("go-tools.debug").start()]]
   vim.cmd [[command! GoDebugStop lua require("go-tools.debug").stop()]]
