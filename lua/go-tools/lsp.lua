75944034c9ccc374781ddf9ff476123a02f3c55c
Huy Duong <huy.duongdinh@gmail.com>
2021-10-17 23:13:21 +0700

    Better way to config lsp

--- lua/go-tools/lsp.lua
+++ lua/go-tools/lsp.lua
@@ -1,20 +1,24 @@
 local M = {}
 
 function M.setup(common_on_attach, common_capabilities, common_on_init)
-  local status, lspconfig = pcall(require, "lspconfig")
-
+  local status, lsp_installer = pcall(require, "nvim-lsp-installer")
   if not status then
     return
   end
 
-  lspconfig.gopls.setup {
-    cmd = {
-      vim.fn.stdpath "data" .. "/lsp_servers/go/gopls",
-    },
-    on_attach = common_on_attach,
-    on_init = common_on_init,
-    capabilities = common_capabilities,
-  }
+  local ok, server = lsp_installer.get_server "gopls"
+  if not ok then
+    return
+  end
+
+  if server:is_installed() then
+    server:setup {
+      on_attach = common_on_attach,
+      on_init = common_on_init,
+      capabilities = common_capabilities,
+    }
+    vim.cmd [[ do User LspAttachBuffers ]]
+  end
 end
 
 return M
